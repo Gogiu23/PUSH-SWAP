94c04a7b915550be488998177223492fdc645587
Giuliano Dominici <gdominic@car3s3.42barcelona.com>
2022-08-10 22:03:05 +0200

    done mallocs need filters for numbers

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: gdominic <gdominic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/30 21:58:45 by gdominic          #+#    #+#             */
-/*   Updated: 2022/08/10 02:18:34 by gdominic         ###   ########.fr       */
+/*   Updated: 2022/08/10 19:47:27 by gdominic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 int main(int argc, char **argv)
 {
-	t_values  *pt1;
+	t_values  pt1;
 	t_values  lenght;
 
 	if (argc <= 1)
@@ -23,29 +23,36 @@ int main(int argc, char **argv)
 		ft_printf("Error\n");
 		return (0);
 	}
-	lenght.lenght = argc;
-	ft_printf("\nNumero de argumentos pasados: %d\n", argc);
+	lenght.lenght = argc - 1;
+	ft_printf("\nNumero de argumentos pasados: %d\n", lenght.lenght);
 
 //	checkarguments(*argv);
 	int	i;
-	int	j;
+	int count;
 
-	j = 1;
+	count = 0;
 	i = 1;
-	pt1 = 0;
 
-	pt1->numbers = malloc(sizeof(char) * argc);
-	if (!pt1->numbers)
+	pt1.numbers = (int **)malloc(sizeof(argc));
+	if (!pt1.numbers)
 		return (0);
-	while (argv[i] != NULL)
+	pt1.numbers[argc] = NULL;
+	while (count < argc)
 	{
-		pt1->numbers[j][i] = ft_atoi(argv[i]);
-		i++;
-		j++;
+		pt1.numbers[count] = (int *)malloc(sizeof(int) * 2);
+		count++;
 	}
-//	ft_printf("Esto es argv: %c\n", pt.x);
-	ft_printf("\nest es el x: %c", pt1->numbers[1]);
-	ft_printf("\n%c", pt1->numbers[3]);
+	if (!pt1.numbers)
+		return (0);
+	count = 1;
+	while (count < argc)
+	{
+		pt1.numbers[0][count] = ft_atoi(argv[count]);
+		ft_printf("\nesto es argv: %d\n", pt1.numbers[0][count]);
+		count++;
+	}
+	ft_printf("\nest es el x: %d", pt1.numbers[0][1]);
+	ft_printf("\nEste es argv en la segunda posicion: %d", pt1.numbers[0][3]);
 	return (0);
 }
 
