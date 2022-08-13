be9a79d8ae00127f15324b40fd6d5c346377c4f8
Alessandro Dominici <dominicipoinelli@Alessandros-MacBook-Pro.local>
2022-08-13 06:28:55 +0200

    clean the printfs and add a new filter

--- push_swap.c
+++ push_swap.c
@@ -12,35 +12,30 @@
 
 #include "./libft/libft.h"
 #include "push_swap.h"
+#include <errno.h>
 
 int main(int argc, char **argv)
 {
 	t_values  pt1;
 	t_values  lenght;
-
-	if (argc <= 1)
-	{
-		ft_printf("Error\n");
-		return (0);
-	}
-	lenght.lenght = argc - 1;
-	ft_printf("\nNumero de argumentos pasados: %d\n", lenght.lenght);
-
-//	checkarguments(*argv);
 	int count;
 	int	i;
 
+	lenght.lenght = argc - 1;
 	i = 1;
 	count = 0;
-	ft_printf("El valor de argc antes del malloc grande: %d\n", argc);
+	
+	if (argc <= 2)
+	{
+		write(1, "Error\n", 6);
+		return (0);
+	}
 	pt1.numbers = (int **)malloc(sizeof(int *) * argc);
 	if (!pt1.numbers)
 		return (0);
 	pt1.numbers[argc] = 0;
-	ft_printf("Il valore di pt1.numbers[%d]\n", argc);
 	while (count < lenght.lenght)
 	{
-		ft_printf("valor de count: %d\t Valor de argc: %d\n", count, argc);
 		pt1.numbers[count] = (int *)malloc(sizeof(int) * 3);
 		if (!pt1.numbers[count])
 		{
@@ -49,14 +44,9 @@ int main(int argc, char **argv)
 		}
 		pt1.numbers[count][0] = ft_atoi(argv[i]);
 		pt1.numbers[count][2] = 0;
-		ft_printf("\nesto es argv[%d]: %d\n", count, pt1.numbers[count][0]);
 		count++;
 		i++;
 	}
-	ft_printf("\neste es el elemento de la fila 2 en la primera columna: %d", pt1.numbers[1][0]);
-	ft_printf("\nEste es argv en la primera posicion: %d", pt1.numbers[0][0]);
-	ft_printf("\nTamaño de tu array: %d\n", sizeof(pt1.numbers));
-	ft_printf("\nEl tamaño de tus filas: %d", sizeof(pt1.numbers)/sizeof(*pt1.numbers[0]));
 	return (0);
 }
 
@@ -70,4 +60,3 @@ int	ft_free(t_values pt1, int count)
 	free(pt1.numbers);
 	return (0);
 }
-//void  checkarguments(char *argv
