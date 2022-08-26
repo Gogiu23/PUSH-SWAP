7f6c70b5eb0c12440787770044f5412a754da933
Gogiu23 <giuliandominici@gmail.com>
2022-08-26 06:26:06 +0200

    Added the sort for three numbers but we exceed the 25 lines. We need to fix it

--- ft_sort.c
+++ ft_sort.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_sort_two.c                                      :+:      :+:    :+:   */
+/*   ft_sort.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gdominic <gdominic@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/24 20:19:56 by gdominic          #+#    #+#             */
-/*   Updated: 2022/08/25 21:45:54 by gdominic         ###   ########.fr       */
+/*   Created: 2022/08/26 04:51:29 by gdominic          #+#    #+#             */
+/*   Updated: 2022/08/26 06:24:47 by gdominic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,4 +35,31 @@ void  ft_sort_three(t_stack a, int argc)
 	int count;
 
 	count = 0;
+   if (a.numbers[count][0] < a.numbers[count + 1][0]
+		   && a.numbers[count + 1][0] < a.numbers[count + 3][0])
+		exit (1);
+   if (a.numbers[count][0] > a.numbers[count + 1][0]
+		   && a.numbers[count][0] < a.numbers[count + 2][0])
+	   ft_sa(a, argc);
+   if (a.numbers[count + 1][0] < a.numbers[count + 2][0])
+		ft_ra(a, argc);
+   if (a.numbers[count][0] > a.numbers[count + 1][0]
+		   && a.numbers[count + 1][0] > a.numbers[count + 2][0])
+	{
+		ft_ra(a, argc);
+		ft_sa(a, argc);
+	}
+   if (a.numbers[count][0] < a.numbers[count + 1][0]
+		   && a.numbers[count][0] > a.numbers[count + 2][0])
+	{
+		ft_ra(a, argc);
+		ft_ra(a, argc);
+	}
+   if (a.numbers[count][0] < a.numbers[count + 1][0]
+   		&& a.numbers[count + 1][0] > a.numbers[count + 2][0])
+   {
+   	ft_pb(a, argc);
+   	ft_sa(a, argc);
+   	ft_pa(a, argc);
+   }
 }
