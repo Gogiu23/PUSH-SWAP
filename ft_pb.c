98de00e8a0c68ce3ee518070d9425dad454a8ec4
Gogiu23 <giuliandominici@gmail.com>
2022-09-07 20:43:52 +0200

    Few changes but not lucky

--- ft_pb.c
+++ ft_pb.c
@@ -6,7 +6,7 @@
 /*   By: gdominic <gdominic@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 04:23:45 by gdominic          #+#    #+#             */
-/*   Updated: 2022/09/05 20:19:04 by gdominic         ###   ########.fr       */
+/*   Updated: 2022/09/06 19:10:47 by gdominic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,15 @@ void	ft_pb(t_stack a, t_stack b, int argc, int counter)
 
 	rows = 0;
 	ft_printf("Entramos en el pb\n");
-	
 	count = (argc - 3);
-	if (b.numbers[rows])
-		b.numbers[rows + 1] = b.numbers[rows];
+	if (b.numbers[rows] != 0)
+		b.numbers[rows]++;
 	ft_printf("Valor de counter: %d\n", counter);
 	b.numbers[rows] = a.numbers[counter];
 	while (count >= 0)
 	{
 		ft_printf("Entramos en el pb\n");
-		a.numbers[count] = a.numbers[count + 1];
+		a.numbers[count][0] = a.numbers[count + 1][0];
 		count--;
 	}
 	ft_printf("pb\n");
